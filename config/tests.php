9f5984fb83b73976e176181e14f5e74e7afc4738
Mehdi Achour <machour@gmail.com>
2018-12-28 13:01:22 +0100

    Fixing tests (#3)
    * Fix tests
    
    * Use wikimedia/composer-merge-plugin
    
    * fix json

---
+++ config/tests.php
@@ -0,0 +1,23 @@
+<?php
+
+$_ENV['TEST_RUNTIME_PATH'] = $_ENV['TEST_RUNTIME_PATH'] ?? dirname(__DIR__) . '/runtime';
+
+return [
+    'app' => [
+        'id' => 'testapp',
+        'aliases' => [
+            '@webroot'           => '@yii/twig/tests/assets',
+            '@runtime'           => $_ENV['TEST_RUNTIME_PATH'],
+            '@app'               => '@yii/twig/tests/',
+            '@app/views'         => '@yii/twig/tests/views',
+            '@app/modules'       => '@yii/twig/tests/views',
+            '@app/widgets'       => '@yii/twig/tests/views',
+            '@yii/tests/runtime' => $_ENV['TEST_RUNTIME_PATH'],
+        ],
+    ],
+    'assetManager' => [
+        '__class'   => yii\web\AssetManager::class,
+        'basePath'  => '@webroot',
+        'baseUrl'   => '@web/assets',
+    ]
+];
