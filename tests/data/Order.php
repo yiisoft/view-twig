ca4c987ad6e6de9a1b90b6068f775119c52ecb24
Andrii Vasyliev <sol@hiqdev.com>
2018-12-28 14:48:04 +0000

    Removed useless use of active record in tests

--- tests/data/Order.php
+++ tests/data/Order.php
@@ -2,9 +2,6 @@
 
 namespace yii\twig\tests\data;
 
-use yii\activerecord\ActiveRecord;
-use yii\db\Connection;
-
 /**
  * Class Order
  *
@@ -13,40 +10,10 @@ use yii\db\Connection;
  * @property integer $created_at
  * @property string $total
  */
-class Order extends ActiveRecord
+class Order extends \yii\base\Model
 {
-    public static $db;
-
-    public static function getDb()
-    {
-        if (static::$db === null) {
-            static::$db = new Connection();
-            static::$db->dsn = 'sqlite::memory:';
-        }
-        return static::$db;
-    }
-
-    public static function setUp()
-    {
-        static::getDb()->createCommand(<<<SQL
-CREATE TABLE "order" (
-  id INTEGER NOT NULL,
-  customer_id INTEGER NOT NULL,
-  created_at INTEGER NOT NULL,
-  total decimal(10,0) NOT NULL,
-  PRIMARY KEY (id)
-);
-SQL
-        )->execute();
-    }
-
-    public static function tableName()
-    {
-        return 'order';
-    }
-
-    public function getCustomer()
-    {
-        return $this->hasOne(Order::class, ['id' => 'customer_id']);
-    }
+    public $id;
+    public $customer_id;
+    public $created_at;
+    public $total;
 }
