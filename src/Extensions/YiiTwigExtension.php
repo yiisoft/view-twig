6880a43080bb227ace4f90ecbfe41b13f8288e35
Chris Yates <beastbytes@users.noreply.github.com>
2022-12-07 16:59:45 +0000

    Add functions to YiiTwigExtension
    Add functions to allow calling Yii static class methods from Twig views
    
    Example:
    
    {% set form = html('form').post(action) %}
    
    {% autoescape false %}
        {{ form.open() }}
            {{ form_field('text', model, 'name').autofocus() }}
            {{ form_field('textarea', model, 'content') }}
            {{ form_field('checkbox', model, 'enabled') }}
    
            {{ form_field('submitButton').content('Save') }}
        {{ form.close() }}
    {% endautoescape %}

--- src/Extensions/YiiTwigExtension.php
+++ src/Extensions/YiiTwigExtension.php
@@ -31,6 +31,70 @@ final class YiiTwigExtension extends AbstractExtension
                 /** @return mixed */
                 fn (string $id) => $this->container->get($id),
             ),
+            new TwigFunction(
+                'array_helper',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Arrays\\ArrayHelper', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
+            new TwigFunction(
+                'array_sort',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Arrays\\ArraySorter', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
+            new TwigFunction(
+                'file_helper',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Files\\FileHelper', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
+            new TwigFunction(
+                'form_field',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Form\\Field', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
+            new TwigFunction(
+                'html',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Html\\Html', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
+            new TwigFunction(
+                'json',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Json\\Json', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
+            new TwigFunction(
+                'numeric_helper',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Strings\\NumericHelper', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
+            new TwigFunction(
+                'string_helper',
+                fn (string $method, mixed ...$args) => forward_static_call_array(
+                    ['\\Yiisoft\\Strings\\StringHelper', $method],
+                    $args
+                ),
+                ['is_variadic' => true]
+            ),
         ];
     }
 }
